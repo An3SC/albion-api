85ab0d0e7a172008b637f84b3457b119cfdbc20e
An3SC <asierracardalda@outlook.es>
2021-06-15 18:14:29 +0200

    implemented last kills and deaths stats

---
+++ src/components/LatestStats/LatestDeaths.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const LatestDeaths = ({ id }) => {
+    const [deaths, setDeaths] = useState(null);
+
+    useEffect(() => {
+        fetch(`/players/${id}/deaths`, {
+            'Content-Type': 'application/json',
+        })
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw response;
+            })
+            .then((data) => {
+                setDeaths(data);
+            })
+            .catch((error) => console.log('Error fetching data: ', error));
+        // eslint-disable-next-line
+    }, [id]);
+
+    function ip(power) {
+        const ip =
+            Math.abs(power) > 999
+                ? Math.sign(power) * (Math.abs(power) / 1000).toFixed(2) + 'k'
+                : Math.round(Math.sign(power) * Math.abs(power));
+        return ip;
+    }
+
+    // console.log(deaths);
+
+    return (
+        <div>
+            <h3>Latest deaths:</h3>
+            <hr />
+            {deaths &&
+                deaths.map((d) => (
+                    <Link
+                        to={`/event/${d.EventId}`}
+                        key={d.BattleId}
+                        className="deathCard"
+                    >
+                        <ul>
+                            <li>Killer: {d.Killer.Name}</li>
+                            <li>Guild: {d.Killer.GuildName}</li>
+                            <li>IP: {ip(d.Killer.AverageItemPower)}</li>
+                        </ul>
+                    </Link>
+                ))}
+        </div>
+    );
+};
+
+export default LatestDeaths;
---
+++ src/components/LatestStats/LatestKills.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const LatestKills = ({ id }) => {
+    const [kills, setKills] = useState(null);
+
+    useEffect(() => {
+        fetch(`/players/${id}/kills`, {
+            'Content-Type': 'application/json',
+        })
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw response;
+            })
+            .then((data) => {
+                setKills(data);
+            })
+            .catch((error) => console.log('Error fetching data: ', error));
+        // eslint-disable-next-line
+    }, [id]);
+
+    function ip(power) {
+        const ip =
+            Math.abs(power) > 999
+                ? Math.sign(power) * (Math.abs(power) / 1000).toFixed(2) + 'k'
+                : Math.round(Math.sign(power) * Math.abs(power));
+        return ip;
+    }
+
+    // console.log(kills);
+
+    return (
+        <div>
+            <h3>Latest kills:</h3>
+            <hr />
+            {kills &&
+                kills.map((k) => (
+                    <Link
+                        to={`/kill/${k.EventId}`}
+                        key={k.BattleId}
+                        className="deathCard"
+                    >
+                        <ul>
+                            <li>Victim: {k.Victim.Name}</li>
+                            <li>Guild: {k.Victim.GuildName}</li>
+                            <li>IP: {ip(k.Victim.AverageItemPower)}</li>
+                        </ul>
+                    </Link>
+                ))}
+        </div>
+    );
+};
+
+export default LatestKills;
